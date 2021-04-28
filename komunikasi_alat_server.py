d7f530120aa42a4a5703e79ec950a9af4b68f403
Hafizh Ry <hafizhrahmatdiantoyusuf379@gmail.com>
2021-04-28 19:41:41 +0700

    versi stabil 2, bisa multiclient connection

--- komunikasi_alat_server.py
+++ komunikasi_alat_server.py
@@ -4,22 +4,21 @@ import threading
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 5050        # The port used by the server
-#ThreadCount = 0
 
-print_lock = threading.Lock()
 
-def threaded(ssocket):
+def threaded(ssocket, list_messages):
     while True:
         data = ssocket.recv(10240)
-        print ("Server received data:", data)
-        #return data.decode()
+        print ("Server received data:", data.decode('utf-8'))
+        list_messages.append(data.decode('utf-8'))
         if not data:
-            print_lock.release()
             break
-        ssocket.sendall(data)
     ssocket.close()
+    print('Socket closed')
 
 def listener_server():
+    ThreadCount = 0
+    list_messages=[]
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         print('Socket binded to port ', PORT)
@@ -28,14 +27,16 @@ def listener_server():
         
         while True:
             ssocket, addr = s.accept() #ssocket stands for server socket
-            print_lock.acquire()
             print('Connected to : ', addr[0], ' : ', addr[1])
-            start_new_thread(threaded, (ssocket,))
-            #ThreadCount += 1
+            start_new_thread(threaded, (ssocket,list_messages))
+            ThreadCount += 1
+            print('Thread Number : ' + str(ThreadCount))
+            print(list_messages)
         s.close()
+    return list_messages
         
 if __name__ == '__main__':
     while True:
-        listener_server()
-                
+        mes = listener_server()
+        print(mes)  
 
