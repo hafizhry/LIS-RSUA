2bf6a13c252ffd97a622d31224e1aa48209fdeb5
hafizhry <hafizhrahmatdiantoyusuf379@gmail.com>
2021-05-05 12:02:18 +0700

    mengembalikan ke versi sebelumnya

--- komunikasi_alat_server.py
+++ komunikasi_alat_server.py
@@ -1,49 +1,33 @@
-  
+from _thread import *
 import socket
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 5050        # The port used by the server
 
-
-def threaded(ssocket, list_messages):
+def threaded(ssocket):
     while True:
-        data = ssocket.recv(10240)
-        print ("Server received data:", data.decode('utf-8'))
-        list_messages.append(data.decode('utf-8'))
+        data = ssocket.recv(2048)
+        print ("Server received data:", data)
         if not data:
             break
+        ssocket.sendall(data)
     ssocket.close()
-    print('Socket closed')
 
 def listener_server():
     ThreadCount = 0
-    list_messages=[]
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
-        s.listen(5)
-        ssocket, addr = s.accept() #ssocket stands for server socket
-        with ssocket:
-            s.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 10000, 3000))
-            print('Connected to ', addr)
-            while True:
-                data = ssocket.recv(10240)
-                return data.decode()
-                if not data:
-                    break
-                ssocket.sendall(data.decode())
-        
+        s.listen(5)        
         while True:
             ssocket, addr = s.accept() #ssocket stands for server socket
-            print('Connected to : ', addr[0], ' : ', addr[1])
-            start_new_thread(threaded, (ssocket,list_messages))
+            print('Connected to:', addr[0], ':', addr[1])
+            start_new_thread(threaded, (ssocket,))
             ThreadCount += 1
-            print('Thread Number : ' + str(ThreadCount))
-            print(list_messages)
+            print('Thread Number: ' + str(ThreadCount))
         s.close()
-    
         
 if __name__ == '__main__':
     while True:
         listener_server()
-          
+                
 
