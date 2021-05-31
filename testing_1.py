861f3d8e93d4034ca93a5eee9501322372b2eb6b
hafizhry <hafizhrahmatdiantoyusuf379@gmail.com>
2021-05-31 19:58:09 +0700

    final version

--- testing_1.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-try:
-  cnx = mysql.connector.connect(user='root', database='laravel_crud2')
-  cursor = cnx.cursor()
-  cursor.execute("SELECT * FORM employees")   # Syntax error in query
-  cnx.close()
-except mysql.connector.Error as err:
-  print("Something went wrong: {}".format(err))
